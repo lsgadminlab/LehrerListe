4354eda92481423b0cdd2a0154a464ec878806c9
sycrw <timmartinkausemann@gmail.com>
2023-07-24 11:56:37 +0000

    bloat

--- frontend/app/components/InputTicket.tsx
+++ frontend/app/components/InputTicket.tsx
@@ -1,8 +1,19 @@
 "use client";
 
-const InputTicket = () => {
+const InputTicket = (  show: boolean;
+  onHide: () => void;
+  person: PersonDataFlat;
+  mode: "create" | "edit";) => {
   return (
-    
+    <dialog className="modal modal-bottom sm:modal-middle modal-open">
+        <form method="dialog" className="modal-box">
+          <h3 className="font-bold text-lg mb-1">
+            {props.mode == "edit" ? "Edit Person" : "Create Person"}
+          </h3>
+         
+          </div>
+        </form>
+      </dialog>
   );
 };
 
